291e510b0115c05dd5007cd728389428db654ef2
Eliesser Freites <freiteseliesser@hotmail.com>
2025-03-08 18:57:59 +0100

    Add product card component and integrate it into the home page layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
         montserrat: ["Montserrat", "sans-serif"],
       },
     },
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
